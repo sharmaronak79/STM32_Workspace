c8c9897cbd1f6b09d2f09aeb40f70ba3b89bc326
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2022-09-16 12:56:36 -0400

    Update stm32f407xx_spi_driver.c

--- STM32F4xx_drivers/drivers/Inc/Src/stm32f407xx_spi_driver.c
+++ STM32F4xx_drivers/drivers/Inc/Src/stm32f407xx_spi_driver.c
@@ -180,6 +180,14 @@ void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi){
 }
 
 
+void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnorDi){
+	if(EnorDi == ENABLE){
+
+			pSPIx-> CR1 |=(1  << SPI_CR1_SSI);
+		}else{
+			pSPIx -> CR1 &= ~(1 << SPI_CR1_SSI);
+		}
+}
 
 
 
