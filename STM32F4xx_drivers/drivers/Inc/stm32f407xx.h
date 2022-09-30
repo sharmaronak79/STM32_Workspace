9099b8391e094cd80a3d2cf485ebb726c63e264c
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2022-09-30 16:49:57 -0400

    Update stm32f407xx.h

--- STM32F4xx_drivers/drivers/Inc/stm32f407xx.h
+++ STM32F4xx_drivers/drivers/Inc/stm32f407xx.h
@@ -247,6 +247,19 @@ typedef struct{
 	__vo uint32_t FLTR;
 }I2C_RegDef_t;
 
+/*
+ * peripheral register definition structure for USART
+ */
+typedef struct
+{
+	__vo uint32_t SR;         /*!< TODO,     										Address offset: 0x00 */
+	__vo uint32_t DR;         /*!< TODO,     										Address offset: 0x04 */
+	__vo uint32_t BRR;        /*!< TODO,     										Address offset: 0x08 */
+	__vo uint32_t CR1;        /*!< TODO,     										Address offset: 0x0C */
+	__vo uint32_t CR2;        /*!< TODO,     										Address offset: 0x10 */
+	__vo uint32_t CR3;        /*!< TODO,     										Address offset: 0x14 */
+	__vo uint32_t GTPR;       /*!< TODO,     										Address offset: 0x18 */
+} USART_RegDef_t;
 
 
 
@@ -607,8 +620,76 @@ typedef struct{
 #define I2C2_REG_RESET()        				    do{ RCC->APB1RSTR |= (1<<22);	    RCC->APB1RSTR &= ~(1<<22);}while(0)
 #define I2C3_REG_RESET()        				    do{ RCC->APB1RSTR |= (1<<23);	    RCC->APB1RSTR &= ~(1<<23);}while(0)
 
+/******************************************************************************************
+ *Bit position definitions of USART peripheral
+ ******************************************************************************************/
+
+/*
+ * Bit position definitions USART_CR1
+ */
+#define USART_CR1_SBK					0
+#define USART_CR1_RWU 					1
+#define USART_CR1_RE  					2
+#define USART_CR1_TE 					3
+#define USART_CR1_IDLEIE 				4
+#define USART_CR1_RXNEIE  				5
+#define USART_CR1_TCIE					6
+#define USART_CR1_TXEIE					7
+#define USART_CR1_PEIE 					8
+#define USART_CR1_PS 					9
+#define USART_CR1_PCE 					10
+#define USART_CR1_WAKE  				11
+#define USART_CR1_M 					12
+#define USART_CR1_UE 					13
+#define USART_CR1_OVER8  				15
 
 
+
+/*
+ * Bit position definitions USART_CR2
+ */
+#define USART_CR2_ADD   				0
+#define USART_CR2_LBDL   				5
+#define USART_CR2_LBDIE  				6
+#define USART_CR2_LBCL   				8
+#define USART_CR2_CPHA   				9
+#define USART_CR2_CPOL   				10
+#define USART_CR2_STOP   				12
+#define USART_CR2_LINEN   				14
+
+
+/*
+ * Bit position definitions USART_CR3
+ */
+#define USART_CR3_EIE   				0
+#define USART_CR3_IREN   				1
+#define USART_CR3_IRLP  				2
+#define USART_CR3_HDSEL   				3
+#define USART_CR3_NACK   				4
+#define USART_CR3_SCEN   				5
+#define USART_CR3_DMAR  				6
+#define USART_CR3_DMAT   				7
+#define USART_CR3_RTSE   				8
+#define USART_CR3_CTSE   				9
+#define USART_CR3_CTSIE   				10
+#define USART_CR3_ONEBIT   				11
+
+/*
+ * Bit position definitions USART_SR
+ */
+
+#define USART_SR_PE        				0
+#define USART_SR_FE        				1
+#define USART_SR_NE        				2
+#define USART_SR_ORE       				3
+#define USART_SR_IDLE       			4
+#define USART_SR_RXNE        			5
+#define USART_SR_TC        				6
+#define USART_SR_TXE        			7
+#define USART_SR_LBD        			8
+#define USART_SR_CTS        			9
+
+#include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_spi_driver.h"
 #include "stm32f407xx_i2c_driver.h"
