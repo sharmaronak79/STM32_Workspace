93856f31fbb7213bc14f5198f0d5f11b11163d27
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2022-11-15 23:37:37 -0500

    Create test2.c

---
+++ Temperature_Control/test2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    
+    
+    for(int TACH=983;TACH>436;TACH--){
+        int RPM = 3932160/TACH;
+        printf("%d \n ",RPM);
+    }
+    //TACH++;
+
+return 0;
+
+}
